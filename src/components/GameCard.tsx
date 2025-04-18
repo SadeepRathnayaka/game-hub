b0e65451521e0ea8b13f68254248923b1d189bba
Madhushan R.M.S <sadeepm20@gmail.com>
2025-04-18 13:50:06 +0530

    Solved no games for some platforms issue

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,29 +1,29 @@
-import { Game } from "@/hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
-import PlatformIconList from "./PlatformIconList";
+import React from "react";
+import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
-import getCroppedImageUrl from "@/services/image-url";
 import Emoji from "./Emoji";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
 }
+
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
-            platforms={game.parent_platforms.map(
-              (platform) => platform.platform
-            )}
+            platforms={game.parent_platforms?.map((p) => p.platform)}
           />
-          <CriticScore score={game.metacritic}></CriticScore>
+          <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          {" "}
-          {game.name} <Emoji rating={game.rating_top} />{" "}
+          {game.name}
+          <Emoji rating={game.rating_top} />
         </Heading>
       </CardBody>
     </Card>
